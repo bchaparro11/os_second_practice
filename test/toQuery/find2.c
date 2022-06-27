b72d9562a29b5de1c99c6b9ca19f2673d0460737
bchaparro11 <bchaparro@unal.edu.co>
2022-06-27 16:50:25 -0500

    Comments in client are done

--- test/toQuery/find2.c
+++ test/toQuery/find2.c
@@ -1,33 +1,18 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include"../header.h"
-
-
-
-
-
-
-
-//to do socket()
-#include <sys/types.h>
+#include <sys/types.h> //Para ejecutar socket() junto con la siguiente librería
 #include <sys/socket.h>
-//to do struct
-#include <netinet/in.h>
-//to do bzero() connecting
-#include <strings.h>
-#include <string.h>
-//to listen()
-#define BACKLOG 8
-//to do close()
-#include <unistd.h>
+#include <netinet/in.h> //Para usar sockaddr_in
+#include <strings.h> //Para ejecutar bzero()
+//#include <string.h>
+#include <unistd.h> //Para ejecutar close()
+//#include <netdb.h>
+#include <arpa/inet.h> //Para ejecutar inet_ntop()
+#include <pthread.h> //Para ejecutar todo lo que tiene que ver con threads
 
-//additional
-#include <netdb.h>
-#include <arpa/inet.h>
-#include <pthread.h>
-
-#define NUM_HILOS 3
-
+#define NUM_HILOS 1 //Este número se usa también para el BACKLOG en la función listen()
+#define PORT 3535   //Puerto por defecto para la comunicación entre server y client
 
 struct server_information{
     int serverfd, addrlen;
@@ -35,11 +20,93 @@ struct server_information{
     struct sharerow sr1;
 };
 
+int threading(int serverfd,int addrlen, struct sockaddr_in server);
+
+int * connecting(void * server_information);
+
+int searching(struct sharerow sr1, int clientfd);
+
+int main(){
+
+    int serverfd;
+    struct sockaddr_in server;
+    int r,opt=1;
+    int addrlen = sizeof(server);
+    
+    serverfd = socket(AF_INET,SOCK_STREAM,0);
+    if(serverfd<0){
+        perror("Error en socket()\n");
+        exit(0);
+    }
+
+    r = setsockopt(serverfd,SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt));
+    if(r < 0){
+        perror("Error en setsockopt()\n");
+        exit(0);
+    }
+
+    server.sin_family = AF_INET;
+    server.sin_port = htons(PORT);
+    server.sin_addr.s_addr = INADDR_ANY;
+    bzero(server.sin_zero,0);
+
+    r = bind(serverfd,(struct sockaddr *)&server, sizeof(server));
+    if(r<0){
+        perror("Error en bind()\n");
+        exit(0);
+    }
+    
+    r = listen(serverfd,NUM_HILOS);
+    if(r<0){
+        perror("Error en listen()\n");
+        exit(0);
+    }
+
+    threading(serverfd,addrlen,server);
+
+    close(serverfd);
+}
+
+int threading(int serverfd,int addrlen, struct sockaddr_in server){
+
+    pthread_t hilo[NUM_HILOS];
+    struct server_information inf1[NUM_HILOS];
+    int *rh0, i;
+
+    for(int i = 0; i < NUM_HILOS; i++){
+        inf1[i].serverfd = serverfd ;
+        inf1[i].addrlen = addrlen;
+        inf1[i].server = server;
+        struct sharerow sr1;
+        inf1[i].sr1 = sr1;
+    }
+
+    for(int i=0; i<NUM_HILOS; i++){
+        int r = pthread_create(&hilo[i], NULL, (void *)connecting, (void *)&inf1[i]);
+        if(r !=0){
+            perror("\n-->pthread_create erro: ");
+            exit(0);
+        }
+    }
+
+    for(int i=0; i<NUM_HILOS; i++){
+        int r = pthread_join(hilo[i], (void **)&rh0);
+        if(r!=0){
+            perror("\n-->pthread_join error: ");
+            exit(0);
+        }
+    }
+
+    printf("\n");
+    return 0;
+}
+
 int * connecting(void * server_information){
+
     int clientfd,r;
     struct server_information *inf1 = server_information;
     struct sharerow sr1 = inf1->sr1;
-    //clientfd = accept(serverfd,(struct sockaddr *)&client, &socklen);
+    
     clientfd = accept(inf1->serverfd,(struct sockaddr *)&inf1->server, (socklen_t*)&inf1->addrlen);
     if(clientfd<0){
         perror("Error en accept()\n");
@@ -52,7 +119,7 @@ int * connecting(void * server_information){
     inet_ntop( AF_INET, &ipAddr, str, INET_ADDRSTRLEN );
 
     printf("La dirección ip es: %s\n",str);
-    //Se debe poner el número para que funcione 
+   
     r = recv(clientfd,&sr1,sizeof(struct sharerow),0);
     if(r<0){
         perror("Error en recv en server\n");
@@ -65,24 +132,14 @@ int * connecting(void * server_information){
 
     printf("a: %d, b: %d, c: %d\n",sr1.sourceid,sr1.dstid,sr1.hod);
     
-
     searching(sr1,clientfd);
     
     close(clientfd);
 
-    /*
-    struct sharerow sr2;
-    sr2.sourceid = sr1.sourceid + 1;
-    sr2.dstid=sr1.dstid + 1;
-    sr2.hod=sr1.hod + 1;
-
-    printf("a: %d, b: %d, c: %d\n",sr2.sourceid,sr2.dstid,sr2.hod);
-    */
-
-    
 }
 
 int searching(struct sharerow sr1, int clientfd){
+
     int *np=malloc(sizeof(int));
  
     //Si no se escriben datos en interface.bin no es necesario seguir con el código más adelante
@@ -149,109 +206,4 @@ int searching(struct sharerow sr1, int clientfd){
     //printf("\nEsperando el ingreso de cualquier número para terminar el proceso: ");
     //scanf("%hd",&forWait);
     return 0;
-}
-
-int threading(int serverfd,int addrlen, struct sockaddr_in server){
-    pthread_t hilo[NUM_HILOS];
-    struct server_information datosh[NUM_HILOS];
-    int *rh0, i;
-
-    for(int i = 0; i < NUM_HILOS; i++){
-        datosh[i].serverfd = serverfd ;
-        datosh[i].addrlen = addrlen;
-        datosh[i].server = server;
-        struct sharerow sr1;
-        datosh[i].sr1 = sr1;
-    }
-
-    for(int i=0; i<NUM_HILOS; i++){
-        int r = pthread_create(&hilo[i], NULL, (void *)connecting, (void *)&datosh[i]);
-        if(r !=0){
-            perror("\n-->pthread_create erro: ");
-            exit(0);
-        }
-    }
-
-    for(int i=0; i<NUM_HILOS; i++){
-        int r = pthread_join(hilo[i], (void **)&rh0);
-        if(r!=0){
-            perror("\n-->pthread_join error: ");
-            exit(0);
-        }
-    }
-
-    printf("\n");
-    return 0;
-}
-
-int main(){
-    //struct sharerow sr1;
-
-    int serverfd, clientfd;
-    struct sockaddr_in server, client;
-    int r,opt=1;
-    //socklen_t socklen;
-    int addrlen = sizeof(server);
-    char buffer[1024] = { 0 };
-
-    //Aparece un error pero en realidad no hay ningún error con el AF_INET
-    serverfd = socket(AF_INET,SOCK_STREAM,0);
-    if(serverfd<0){
-        perror("Error en socket()\n");
-        exit(0);
-    }
-
-    r = setsockopt(serverfd,SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt));
-    if(r < 0){
-        perror("Error en setsockopt()\n");
-        exit(0);
-    }
-
-    server.sin_family = AF_INET;
-    server.sin_port = htons(3535);
-    server.sin_addr.s_addr = INADDR_ANY;
-    bzero(server.sin_zero,0);
-
-
-    // tener cuidado por si toca hacer casting o no en el tercer argumento
-    //r = bind(serverfd,(struct sockaddr *)&server, sizeof(struct sockaddr));
-    r = bind(serverfd,(struct sockaddr *)&server, sizeof(server));
-    if(r<0){
-        perror("Error en bind()\n");
-        exit(0);
-    }
-    
-    r = listen(serverfd,BACKLOG);
-    if(r<0){
-        perror("Error en listen()\n");
-        exit(0);
-    }
-
-
-    //-----------------------------------------------------------------
-
-    threading(serverfd,addrlen,server);
-    
-
-
-
-
-
-
-    
-
-    //printf("%s\n", buffer);
-    //printf("a: %d, b: %d, c: %d\n",sr1->sourceid,sr1->dstid,sr1->hod);
-
-    /*
-    r =  send(clientfd,"hola mundo",10,0);
-    if(r<0){
-        perror("Error en send()\n");
-        exit(0);
-    }
-    */
-    
-
-    //close(clientfd);
-    close(serverfd);
 }
--- test/toQuery/interface2.c
+++ test/toQuery/interface2.c
@@ -1,116 +1,87 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<unistd.h>
-#include"../header.h"
-
-
-
-
-#include <sys/types.h>          /* See NOTES */
+#include<unistd.h> //Para ejecutar close()
+#include"../header.h" //Para usar los structs específicos par el funcionamiento
+#include <sys/types.h> //Para usar socket() junto con la siguiente
 #include <sys/socket.h>
-//to do struct
-#include <netinet/in.h>
-//to do inet_aton()
-#include <netinet/in.h>
-#include <arpa/inet.h>
-//to do close()
-#include <unistd.h>
-//additional
-#include <netdb.h>
+#include <netinet/in.h> //Para usar sockaddr_in
+#include <arpa/inet.h> //Para usar inet_ntop
+//#include <netdb.h>
+//#include <strings.h>
 
-#include <string.h>
+#define PORT 3535 //Puerto por defecto para la conexión entre client y server
+#define SERVER_IP "127.0.0.1" //Dirección de LOOPBACK para conectar con el server
 
-//defines
-#define PORT 3535
-
-
-
-void creating_client(struct sharerow *sr1);
+void creating_client(struct sharerow *sr1); //Declarando función para crear un cliente
 
 int main(){
-	/*
-	Script para la interface
-	*/
+    /*
+    - Struct utilizado para almacenar la información del menu
+    - La definición del struct está en ../header.h
+    */
+    struct sharerow sr1={0,0,0}; 
 
-    FILE *fshare;
-    fshare = fopen("share.bin","w+b");
-    if(fshare==NULL){
-        printf("Ocurrió un problema con el opening de share.bin!\n");
-    }
-    struct sharerow sr1={0,0,0};
+    short int opcion; //Para poder ejecutar el switch
 
-    short int opcion;
-
-    
     while(1) {
-        system("clear");
+        system("clear"); //Limpiando la consola para que se vea mejor el menú
  		printf("\nBienvenido\n\n"
  			"1.  Ingresar origen\n"
  			"2.  Ingresar destino\n"
  			"3.  Ingresar hora\n"
  			"4.  Buscar tiempo de viaje medio\n"
  			"5.  Salir\n"
- 			"*********************************************\n¿Elige la opción?: ");
+ 			"*********************************************\n¿Elige la opción?: "); //Imprimiendo el menú
 	 			
-	 	scanf("%hd", &opcion);
+	 	scanf("%hd", &opcion); //Recibiendo la opción del usuario
 
 	 	switch(opcion) {
 	 		case 1:
-	  			system("clear");
+	  			system("clear"); //Limpiando la consola para que se vea mejor la interacción en case 1
 	  			printf("\nIngrese ID del origen: ");
-	  			scanf("%hd", &sr1.sourceid);
+	  			scanf("%hd", &sr1.sourceid); //Almacenando el valor ingresado por el usuario en struct sharerow sr1
                 if (sr1.sourceid<0 || sr1.sourceid>1160){
 					system("clear");
-                    printf("\nValor incorrecto, se espera un número entre 1 y 1160\n");
+                    printf("\nValor incorrecto, se espera un número entre 1 y 1160\n"); //Manejando valores incorrectos
                     sleep(3);
                     break;    
                 }
 				break;
 
 	     	case 2:
-	 			system("clear");
+	 			system("clear"); //Limpiando la consola para que se vea mejor la interacción en case 1
 	  			printf("\nIngrese ID del destino: ");
-	  			scanf("%hd", &sr1.dstid);
+	  			scanf("%hd", &sr1.dstid); //Almacenando el valor ingresado por el usuario en struct sharerow sr1
                 if (sr1.dstid<0 || sr1.dstid>1160){
                     system("clear");
-                    printf("\nValor incorrecto, se espera un número entre 1 y 1160\n");
+                    printf("\nValor incorrecto, se espera un número entre 1 y 1160\n"); //Manejando valores incorrectos
                     sleep(3);  
                     break;      
                 }
 				break;
 
 	     	case 3:
-	  			system("clear");
-	  			printf("\nIngrese hora del día: ");
-	  			scanf("%hd",&sr1.hod);
+	  			system("clear"); //Limpiando la consola para que se vea mejor la interacción en case 1
+	  			printf("\nIngrese hora del día: "); 
+	  			scanf("%hd",&sr1.hod); //Almacenando el valor ingresado por el usuario en struct sharerow sr1
                 if (sr1.hod<0 || sr1.hod>23){
                     system("clear");
-                    printf("\nValor incorrecto, se espera un número entre 0 y 23\n");
+                    printf("\nValor incorrecto, se espera un número entre 0 y 23\n"); //Manejando valores incorrectos
                     sleep(3);
                     break;        
                 }
 
 	  			break;
 	     	case 4:
-				system("clear");
-                //fwrite(&sr1,sizeof(struct sharerow),1,fshare);
-                //fclose(fshare);
-                //system("gcc find.c -o find");
-                //char *args[]={"./find",NULL};
-                //execv(args[0],args); //No aparentado
-				//struct client_server_comunication csc = {12345,678,90};
-				//Aquí debería existir el for y sale!
-    			creating_client(&sr1);
-
+				system("clear"); //Limpiando la consola para que se vea mejor la interacción en case 1
+    			creating_client(&sr1); //Creando el cliente. Mirar función para entenderlo
+                //No se pone aposta el break; para que entre al case 5 también
 	     	case 5:
-				//system("clear");
-				//fclose(fshare);
 				printf("\nHasta Luego!\n\n");
 				exit(0);
-				//return(1);	
 	     	default:
-				system("clear");
-                printf("\n¡Esa opción no es válida!\n");
+				system("clear"); //Limpiando la consola para que se vea mejor la interacción en case 1
+                printf("\n¡Esa opción no es válida!\n"); //Manejando valores incorrectos
                 sleep(1);
                 break;
 	 	}
@@ -118,67 +89,44 @@ int main(){
     return 0;
 }
 
-
 void creating_client(struct sharerow *sr1){
-    int clientfd,r,opt=1;
-    struct sockaddr_in client;
-    socklen_t socklen;
-    char buffer[1024] = { 0 };
-    //char buffer[10];
+    int clientfd,r; //clientfd es el descriptor del cliente y r es usado para manejar errores
+    struct sockaddr_in client; //Struct para poder ingresar datos de server y conectarse a él
 
-    char* hello = "Este mensaje salió desde client";
-
-    clientfd = socket(AF_INET, SOCK_STREAM, 0);
+    clientfd = socket(AF_INET, SOCK_STREAM, 0); //Definiendo el cliente
     if(clientfd<0){
-        perror("Error en socket()\n");
+        perror("Error en socket()\n"); //Manejando error de socket()
         exit(0);
     }
-    //setsockopt(clientfd,SOL_SOCKET, SO_REUSEADDR, (const char *)&opt, sizeof(int));
 
-    client.sin_family = AF_INET;
-    client.sin_port = htons(PORT);
-    //r = inet_aton("127.0.0.1",&client.sin_addr);
-    r = inet_pton(AF_INET,"127.0.0.1",&client.sin_addr);
+    client.sin_family = AF_INET; //Para decir que se usará IPV4
+    client.sin_port = htons(PORT); //Usando el puerto 3535 y manejando el endinismo
+
+    r = inet_pton(AF_INET,SERVER_IP,&client.sin_addr); //Para guardar en client.sin_addr la ip del server en binario
     if (r<=0){
-        printf("Error en inet_aton()");
+        printf("Error en inet_aton()");//Manejando error de inet_pton()
     }
-    //NO SE HIZO EL bzero() NO SÉ POR QUÉ
 
-
-    //r = connect(clientfd,(struct sockaddr *)&client,(socklen_t)sizeof(struct sockaddr));
-    r = connect(clientfd,(struct sockaddr *)&client,sizeof(client));
+    r = connect(clientfd,(struct sockaddr *)&client,sizeof(client)); //Conectándose al servidor
     if(r<0){
-        perror("Error en connect\n");
+        perror("Error en connect\n"); //Manejando error de connect()
         exit(0);
     }
 
+    send(clientfd,sr1,sizeof(struct sharerow),0); //Enviando el struct sharerow obtenido en el menú
 
+    struct row r1; //Struct row, que está en ../header.h para recibir toda la información del viaje gestionada por el server
 
-    //To send message
-    //struct sharerow sr1 = {123,987,465};
-
-    //send(clientfd,hello,strlen(hello),0);
-    send(clientfd,sr1,sizeof(struct sharerow),0);
-    struct row r1;
-    r = recv(clientfd, &r1,sizeof(struct row),0);
+    r = recv(clientfd, &r1,sizeof(struct row),0); //Para recibir toda la información del viaje gestionada por el server
     if(r<0){
-        perror("Error en recv\n");
+        perror("Error en recv\n"); //Manejando error de recv()
         exit(0);
     }
     
+    //Las siguientes tres líneas de código son para imprimir la información obtenida en recv()
     printf("\nTiempo de viaje medio: %0.2f\n\n",r1.mean);
     printf("mpos: %d \nnpos: %d\n",r1.mpos,r1.npos);
     printf("sourceid: %d \ndstid: %d \nhod: %d\n",r1.sourceid,r1.dstid,r1.hod);
 
-    //printf("a: %d, b: %d, c: %d\n",r1.sourceid,r1.dstid,r1.hod);
-
-    //printf("El valor de r es: %d\n",r);
-    //buffer[r]=0;
-    //for(int i = 0;i<(sizeof(buffer)/sizeof(char));i++){
-    //    //printf("%d\n",i);
-    //    printf("%c\n",buffer[i]);
-    //}
-    //printf("El mensaje es: \n %s\n",buffer);
-
-    close(clientfd);
+    close(clientfd); //Cerrando el client declarado al principio de esta función
 }
